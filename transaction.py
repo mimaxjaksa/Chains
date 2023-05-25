2512083443cacdda67e15d7a0b95a6438753e99d
Jaksa Jovicic <jovicicjaksa@gmail.com>
2023-05-25 16:21:05 +0200

    Name refactor + repr

--- transaction.py
+++ transaction.py
@@ -1,2 +1,34 @@
 from utils import *
+from wallet import *
 
+class Transaction:
+    def __init__(self, from_w: Wallet, to_w: Wallet, message: str = "mlem", amount: float = 0):
+        self.message = message
+        self.amount = amount
+        self.from_w = from_w
+        self.to_w = to_w
+        self.processed = False
+    
+    def process(self):
+        self.from_w.take_amount(self.amount)
+        self.to_w.add_amount(self.amount)
+        self.processed = True
+    
+    def __repr__(self):
+        return f"Transaction: \n\tmessage: {self.message}, \n\tfrom wallet: {self.from_w.name}, \n\tto wallet: {self.to_w.name}, \n\tamount: {self.amount}, \n\tprocessed: {self.processed}"
+
+
+if __name__ == "__main__":
+    w1 = Wallet("Alice", 10)
+    w2 = Wallet("Bob", 20)
+    print(w1)
+    print(w2)
+
+    t = Transaction(w1, w2, amount=3.2)
+
+    print(t)
+    t.process()
+    print(t)
+
+    print(w1)
+    print(w2)
--- wallet.py
+++ wallet.py
@@ -1,8 +1,9 @@
 from utils import *
 
 class Wallet():
-    def __init__(self, name: str = "mlem"):
+    def __init__(self, name: str = "mlem", amount: float = 0):
         self.name = name
+        self.amount = amount
         random_seed = random.randint(0, 1000)
         # self.private_key = hash_unicode(str(random_seed))
         self.private_key = 0xA0DC65FFCA799873CBEA0AC274015B9526505DAAAED385155425F7337704883E #find a way how to generate this
@@ -10,15 +11,20 @@ class Wallet():
         # self.private_key_bin = ''.join(format(ord(x), 'b') for x in self.private_key)
         # self.private_key_int = int(self.private_key_bin, base=2)
 
+    def take_amount(self, amount):
+        self.amount -= amount
+
+    def add_amount(self, amount):
+        self.amount += amount
+
     def __repr__(self):
-        return repr('BChain ' + self.name)
+        return f"Wallet: \n\tname: {self.name}, \n\tamount: {self.amount}"
 
     def hash256(self):
         return hash_unicode(repr(self))
 
 if __name__ == "__main__":
-    pass
-    w = Wallet()
-    # print(w.private_key)
-    pub_k = ecc_multiply(G, w.private_key)
-    # print(G*w.private_key_int)
+    w1 = Wallet("Alice", 10)
+    w2 = Wallet("Bob", 20)
+    print(w1)
+    print(w2)
